110cc449dfd0eb7cbb6ce0c70decd09fe5cf5a89
Jammie sandy <33603488+jamessandy@users.noreply.github.com>
2026-06-05 02:09:58 +0100

    Load board editor only in browser

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-import { BoardClient } from '@/components/BoardClient'
+'use client'
+
+import dynamic from 'next/dynamic'
+
+const BoardClient = dynamic(() => import('@/components/BoardClient').then((module) => module.BoardClient), {
+  ssr: false,
+  loading: () => <main className="h-screen bg-[#f4f1ea]" />,
+})
 
 export default function Home() {
   return <BoardClient />
